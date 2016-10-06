79cd9cf6978409da96ab122e6f2e34a32c190031
student <student@240-11.INFORMATYKA.local>
2016-10-06 14:51:53 +0200

    zad 4 c

--- src/org/yourorghere/JOGL.java
+++ src/org/yourorghere/JOGL.java
@@ -10,7 +10,13 @@ import javax.media.opengl.GLCanvas;
 import javax.media.opengl.GLEventListener;
 import javax.media.opengl.glu.GLU;
 
-
+/**
+ * JOGL.java <BR>
+ * author: Brian Paul (converted to Java by Ron Cemer and Sven Goethel)
+ * <P>
+ *
+ * This version is equal to Brian Paul's version 1.2 1999/10/21
+ */
 public class JOGL implements GLEventListener {
 
     public static void main(String[] args) {
@@ -25,7 +31,9 @@ public class JOGL implements GLEventListener {
 
             @Override
             public void windowClosing(WindowEvent e) {
-                
+                // Run this on another thread than the AWT event queue to
+                // make sure the call to Animator.stop() completes before
+                // exiting
                 new Thread(new Runnable() {
 
                     public void run() {
@@ -35,30 +43,32 @@ public class JOGL implements GLEventListener {
                 }).start();
             }
         });
-        
+        // Center frame
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
         animator.start();
     }
 
     public void init(GLAutoDrawable drawable) {
-      
+        // Use debug pipeline
+        // drawable.setGL(new DebugGL(drawable.getGL()));
+
         GL gl = drawable.getGL();
         System.err.println("INIT GL IS: " + gl.getClass().getName());
 
-        
+        // Enable VSync
         gl.setSwapInterval(1);
 
-        
+        // Setup the drawing area and shading mode
         gl.glClearColor(0.0f, 0.0f, 0.0f, 0.0f);
-        gl.glShadeModel(GL.GL_SMOOTH);
+        gl.glShadeModel(GL.GL_SMOOTH); // try setting this to GL_FLAT and see what happens.
     }
 
     public void reshape(GLAutoDrawable drawable, int x, int y, int width, int height) {
         GL gl = drawable.getGL();
         GLU glu = new GLU();
 
-        if (height <= 0) { 
+        if (height <= 0) { // avoid a divide by zero error!
 
             height = 1;
         }
@@ -71,26 +81,21 @@ public class JOGL implements GLEventListener {
         gl.glLoadIdentity();
     }
 
-    public void drawTriangleFan(float xsr, float ysr, float r, GL gl) {
-        float kat;
-        gl.glBegin(GL.GL_TRIANGLE_FAN);
-        gl.glVertex3f(xsr, ysr, -6.0f);
-        for (kat = 0.0f; kat < (2.0f * Math.PI);
-                kat += (Math.PI / 32.0f)) {
-            float x = r * (float) Math.sin(kat) + xsr;
-            float y = r * (float) Math.cos(kat) + ysr;
+    public void drawTriangle(float x1, float y1, float x2, float y2, float x3, float y3, float z, GL gl) {
+        gl.glBegin(GL.GL_TRIANGLES);
+        gl.glVertex3f(x3, y3, -6.0f);
+        gl.glVertex3f(x1, y1, -6.0f);
+        gl.glVertex3f(x2, y2, -6.0f);
+        gl.glEnd();
+}
 
-            gl.glVertex3f(x, y, -6.0f);
-        }
-    }
-
-    public void display(GLAutoDrawable drawable) {
+public void display(GLAutoDrawable drawable) {
 
         GL gl = drawable.getGL();
         gl.glClear(GL.GL_COLOR_BUFFER_BIT | GL.GL_DEPTH_BUFFER_BIT);
         gl.glLoadIdentity();
 
-        drawTriangleFan((float) 1.5, (float) 1.5, (float) 0.5, gl);
+        drawTriangle((float) 0.0, (float) -1.0, (float) 2.0, (float) -1.0, (float) 1.0, (float) 1.0, (float) -3.0, gl);
 
         gl.glEnd();
         gl.glFlush();
